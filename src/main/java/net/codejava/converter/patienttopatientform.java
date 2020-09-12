309e2596923e0cbf95b36e2d2fe1c26c2dd6e02b
roshanchaube <chauberoshan@gmail.com>
2020-09-12 09:23:04 +0000

    roshan update

--- src/main/java/net/codejava/converter/patienttopatientform.java
+++ src/main/java/net/codejava/converter/patienttopatientform.java
@@ -5,6 +5,16 @@ import net.codejava.Domain.patient;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-public class patienttopatientform {
-    
+@Component
+public class patienttopatientform implements Converter<patient, patientform> {
+    @Override
+    public patientform convert(patient patient1) {
+        patientform patientform1 = new patientform();
+        patientform1.setId(patient1.get_id().toHexString());
+        patientform1.setFirstname(patient1.getFirstname());
+        patientform1.setLastname(patient1.getLastname());
+        patientform1.setGender(patient1.getGender());
+        patientform1.setBirthyear(patient1.getBirthyear());
+        return patientform1;
+    }
 }
