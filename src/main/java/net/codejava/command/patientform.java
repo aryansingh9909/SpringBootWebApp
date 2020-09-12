d41456992af189b5eb7441576f086d94093a42d4
roshanchaube <chauberoshan@gmail.com>
2020-09-12 07:01:12 +0000

    roshan updated getter setter

---
+++ src/main/java/net/codejava/command/patientform.java
@@ -0,0 +1,50 @@
+package net.codejava.command;
+
+
+public class patientform {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String Gender;
+    private int birthyear;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public int getBirthyear() {
+        return birthyear;
+    }
+
+    public void setBirthyear(int birthyear) {
+        this.birthyear = birthyear;
+    }
+}
